5ec2860d54670b06ae4029b3f3c65c90949e6081
borhansahed <mohammadborhan022@gmail.com>
2024-02-18 10:22:48 +0600

    sovled one problem

---
+++ phase-1/practice-2/searching.cpp
@@ -0,0 +1,32 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin >> n;
+    int a[n];
+    int x;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+
+    cin >> x;
+
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] == x)
+        {
+            cout << i << endl;
+
+            return 0;
+        }
+    }
+
+    cout << -1 << endl;
+    return 0;
+}
