0be7b20581598dc6a0366b5e305c18e47cef2dcf
borhansahed <mohammadborhan022@gmail.com>
2025-07-01 12:42:47 +0600

    calculation funcation problem thinking

--- phase-1/practice-3/calculatingFunction.cpp
+++ phase-1/practice-3/calculatingFunction.cpp
@@ -6,6 +6,17 @@ int main()
     long long n;
     cin >> n;
 
+    /*
+
+    if number is even then print n/2 example:
+    if n = 4 then
+    -1 + 2 - 3 + 4 = 2 , 2 == n/2
+
+    if n = 7 then
+    -1 + 2 -3 + 4 -5 + 6 - 7 =  -4 , -4 == (n/2 + 1)
+
+
+    */
     if (n % 2 == 0)
     {
         cout << (n / 2) << endl;
